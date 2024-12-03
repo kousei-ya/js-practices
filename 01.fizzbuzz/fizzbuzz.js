c66139488bba0e0d7c1d9455a8ac5b5eb8bf646b
kousei-ya <k.yano@genbasupport.com>
2024-12-03 16:15:40 +0900

    fizzbuzzプログラムを作成

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+for (let num = 1; num <= 20; num++) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (num % 5 === 0) {
+    console.log("buzz");
+  } else if (num % 3 === 0) {
+    console.log("fizz");
+  } else {
+    console.log(String(num));
+  }
+}
