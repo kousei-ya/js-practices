f7660b74155aec6cd22152fefa2ba70c8f07c6d0
kousei-ya <k.yano@genbasupport.com>
2024-12-04 09:14:44 +0900

    出力する文字列を正しいものに修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,10 +1,12 @@
+#! /usr/bin/env node
+
 for (let num = 1; num <= 20; num++) {
   if (num % 3 === 0 && num % 5 === 0) {
-    console.log("fizzbuzz");
+    console.log("FizzBuzz");
   } else if (num % 5 === 0) {
-    console.log("buzz");
+    console.log("Buzz");
   } else if (num % 3 === 0) {
-    console.log("fizz");
+    console.log("Fizz");
   } else {
     console.log(String(num));
   }
