b437a81b547b3ee0e98253b823aeb73f90faa057
kousei-ya <k.yano@genbasupport.com>
2025-01-15 11:40:32 +0900

    ファイル名をbase_functions.jsからsqlite_promisified.jsに変更

--- 03.asynchronous/async_with_error.js
+++ 03.asynchronous/async_with_error.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { promisifyDatabase } from "./base_functions.js";
+import { promisifyDatabase } from "./sqlite_promisified.js";
 
 async function main() {
   const db = new sqlite3.Database(":memory:");
